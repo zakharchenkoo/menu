dff2530c96af041e658d5446fe01ccc156b840c6
zakharchenkoo <135759467+zakharchenkoo@users.noreply.github.com>
2023-09-23 19:43:26 +0200

    Add files via upload

---
+++ Menu/index.js
@@ -0,0 +1,424 @@
+const menu = [
+  {
+    id: 1,
+    title: "filet z kurczaka po ukrainsku",
+    category: "dania kozaka",
+    price: 16,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 2,
+    title: "kotlet kozacki",
+    category: "dania kozaka",
+    price: 16,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 3,
+    title: "salatka kozaka",
+    category: "salatki",
+    price: 18,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 4,
+    title: "salatka z grillowanym kurczakiem",
+    category: "salatki",
+    price: 28,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 5,
+    title: "salatka z grillowanym lososiem",
+    category: "salatki",
+    price: 34,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 6,
+    title: "pierogi ukrainskie z twarogiem",
+    category: "pierogi",
+    price: 24,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 7,
+    title: "pierogi ukrainskie z kapusta",
+    category: "pierogi",
+    price: 24,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 8,
+    title: "pielmeni miesne",
+    category: "pierogi",
+    price: 24,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 9,
+    title: "kotlet po francusku",
+    category: "dania kozaka",
+    price: 24,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 10,
+    title: "krokiety z pieczarkami",
+    category: "dania kozaka",
+    price: 24,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 11,
+    title: "filet rybny duszony z wareami po ukrainsku",
+    category: "dania rybne",
+    price: 18,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 12,
+    title: "smazona ryba",
+    category: "dania rybne",
+    price: 18,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 13,
+    title: "borszcz ukrainski z pampuszka",
+    category: "zupy",
+    price: 26,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 14,
+    title: "ukrainska solanka",
+    category: "zupy",
+    price: 26,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 15,
+    title: "pucharek lodowy",
+    category: "desery",
+    price: 14,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 16,
+    title: "tiramisu",
+    category: "desery",
+    price: 15,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 17,
+    title: "kotlet po kijowsku",
+    category: "dania kozaka",
+    price: 20,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 18,
+    title: "kotlet domowy",
+    category: "dania kozaka",
+    price: 16,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 19,
+    title: "surowka z marchewki",
+    category: "surowki",
+    price: 5,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 20,
+    title: "surowka z buraczkow",
+    category: "surowki",
+    price: 5,
+    img: "./images/item-1.jpeg",
+    desc: `description `,
+  },
+  {
+    id: 21,
+    title: "zrazy ukrainskie",
+    category: "dania kozaka",
+    price: 28,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 22,
+    title: "golabki po ukrainsku",
+    category: "dania kozaka",
+    price: 28,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 23,
+    title: "mieso pod ananasem",
+    category: "dania kozaka",
+    price: 22,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 24,
+    title: "swiezonka ukrainska",
+    category: "dania kozaka",
+    price: 28,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 25,
+    title: "ketchup",
+    category: "dodatki",
+    price: 2,
+    img: "./images/item-1.jpeg",
+    desc: `porcja 300 g`,
+  },
+  {
+    id: 26,
+    title: "majonez",
+    category: "dodatki",
+    price: 3,
+    img: "./images/item-1.jpeg",
+    desc: `porcja 300 g`,
+  },
+  {
+    id: 27,
+    title: "sos czosnkowy",
+    category: "dodatki",
+    price: 3,
+    img: "./images/item-1.jpeg",
+    desc: `porcja 300 g`,
+  },
+  {
+    id: 28,
+    title: "smietana",
+    category: "dodatki",
+    price: 3,
+    img: "./images/item-1.jpeg",
+    desc: `porcja 300 g`,
+  },
+  {
+    id: 29,
+    title: "podsmazona cebulka",
+    category: "dodatki",
+    price: 5,
+    img: "./images/item-1.jpeg",
+    desc: `porcja 100 g`,
+  },
+  {
+    id: 30,
+    title: "kompot owocowy",
+    category: "napoje",
+    price: 6,
+    img: "./images/item-1.jpeg",
+    desc: `porcja 250 ml`,
+  },
+   {
+    id: 31,
+    title: "placki nadziewane miesem",
+    category: "dania kozaka",
+    price: 32,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 32,
+    title: "placki w kotle",
+    category: "dania kozaka",
+    price: 32,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 33,
+    title: "ukrainska zupa z klopsikami lub vege",
+    category: "zupy",
+    price: 22,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 34,
+    title: "ukrainska zupa rybna",
+    category: "zupy",
+    price: 26,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 35,
+    title: "goracy garnek po ukrainsku",
+    category: "dania kozaka",
+    price: 32,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 36,
+    title: "placki tradycyjne",
+    category: "dania kozaka",
+    price: 19,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 37,
+    title: "frytki (jako dodatek do dania)",
+    category: "dodatki",
+    price: 8,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 38,
+    title: "frytki (jako danie)",
+    category: "dodatki",
+    price: 12,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+  {
+    id: 39,
+    title: "puree ziemniaczane",
+    category: "dodatki",
+    price: 6,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+      {
+    id: 40,
+    title: "ziemniaki po chlopsku (jako dodatek do dania)",
+    category: "dodatki",
+    price: 10,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+       {
+    id: 41,
+    title: "ziemniaki po chlopsku",
+    category: "dodatki",
+    price: 14,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+        {
+    id: 42,
+    title: "ryz z warzywami",
+    category: "dodatki",
+    price: 6,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+         {
+    id: 43,
+    title: "ryz bialy",
+    category: "dodatki",
+    price: 6,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+          {
+    id: 44,
+    title: "pampuszka",
+    category: "dodatki",
+    price: 6,
+    img: "./images/item-1.jpeg",
+    desc: `description`,
+  },
+];
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayCategoryItems();
+});
+
+const sectionElement = document.querySelector(".section-center");
+const categoryElement = document.querySelector(".btn-container");
+
+function displayMenuItems(menuList) {
+  let sectionContent = "";
+
+  // map ile ?
+  menuList.forEach((item) => {
+    sectionContent += `<article class="menu-item">
+        <img src=${item.img} alt="menu item" class="photo" />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}zł</h4>
+          </header>
+          <p class="item-text">
+            ${item.desc}
+          </p>
+        </div>
+      </article>`;
+  });
+  sectionElement.innerHTML = sectionContent;
+}
+
+function displayCategoryItems() {
+  let categories = [];
+  let categoryContent = `<button type="button" class="filter-btn" data-id="all">wszystko</button>`;
+
+  menu.forEach((item) => {
+    const foodIndex = categories.indexOf(item.category);
+    if (foodIndex === -1) {
+      categories.push(item.category);
+    }
+  });
+
+  categories.forEach((category) => {
+    categoryContent += `<button type="button" class="filter-btn" data-id="${category}">${category}</button>`;
+  });
+
+  categoryElement.innerHTML = categoryContent;
+
+  // =============
+
+  const filterButtons = document.querySelectorAll(".filter-btn");
+
+  filterButtons.forEach((button) => {
+    // event
+    button.addEventListener("click", function (e) {
+      // Yemek filtreleme işlemi gerçekleşecek.
+      const selectedCategoryName = e.target.getAttribute("data-id");
+      const filteredMenu = menu.filter(
+        (item) => item.category === selectedCategoryName
+      );
+
+      console.log("selectedCategoryName: " + selectedCategoryName);
+
+      if (selectedCategoryName == "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(filteredMenu);
+      }
+    });
+  });
+}
